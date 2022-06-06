a2cbe4c944c41c912c31957d3cd60c34657d3ed4
Dicky Darmawan <dickyd6@gmail.com>
2022-06-06 23:20:01 +0700

    Section About

---
+++ src/components/about/Content.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="h-full px-10 mt-auto">
+      <div class="container px-6 mx-auto mb-24 flex odd:flex-row-reverse even:flex-row" v-for="content in contents" :key="content">
+        <div class="basis-1/2 items-center ">
+            <img src="../../assets/img/illustration/exmp.png" alt="">
+        </div>
+        <div class="grid basis-1/2 content-center">
+            <hr class="w-20 rounded-sm border-primary-brand md:h-3 md:border-t-4">
+            <h1 class="text-h3 font-black">{{content.header}}</h1>
+            <span class="pt-2">{{content.content}}</span>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import Content from '../../assets/data/contentAbout.json'
+export default {
+     data() {
+    return {
+      contents: Content,
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/about/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="h-[100vh] pt-24 grid md:flex flex-wrap bg-primary-green-l-20">
+        <div class="container px-6 mx-auto">
+            <div class="grid justify-items-center items-center py-52">
+                <h1 class="font-black text-center text-h2 md:text-left md:max-w-3xl md:text-[5rem]">
+                    Header
+                </h1>
+                <span class="text-center pt-6 text-h6 font-thin max-w-xl">
+                    Body
+                </span>
+            </div>
+        </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
