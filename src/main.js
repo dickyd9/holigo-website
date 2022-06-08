355c8f13c6a2db76b2bb8dfe5f6d6c99dfd1350a
Dicky Darmawan <dickyd6@gmail.com>
2022-06-08 22:46:24 +0700

    change

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import './index.css';
+import store from './stores/index.js';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.use(store).mount("#app");
